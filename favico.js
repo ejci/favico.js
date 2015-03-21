2d5d9d10a0fb41a2929153af1c885277cfc55c83
Miroslav Magda <magda.miroslav@gmail.com>
2015-03-21 15:07:36 +0100

    Removed domain check as its useless and it prevents serving favicon from CDN. To use images from other domain you need to set proper Access-Control-Allow-Origin. Check this https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS Setting proper header will allow this library to handle images also from CDN

--- favico.js
+++ favico.js
@@ -481,11 +481,6 @@
 					document.getElementsByTagName('head')[0].appendChild(elm);
 				}
 			}
-			//check if image and link url is on same domain. if not raise error
-			url = (_opt.elementId) ? elm.src : elm.href;
-			if (url.substr(0, 5) !== 'data:' && url.indexOf(document.location.hostname) === -1) {
-				throw new Error('Error setting favicon. Favicon image is on different domain (Icon: ' + url + ', Domain: ' + document.location.hostname + ')');
-			}
 			elm.setAttribute('type', 'image/png');
 			return elm;
 		};
